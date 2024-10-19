d287eed83e44687f7514dfe964d2ecd7f0a65c01
mh-vasconcelos <66500702+mh-vasconcelos@users.noreply.github.com>
2024-10-19 20:30:41 -0300

    Update dicionario.py

--- dicionario.py
+++ dicionario.py
@@ -1,69 +1,64 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 6,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from imports import *\n",
-    "from main_ws import *"
-   ]
-  },
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": [
-    "### 1 -  bs(requisicao, tipo da requisição)\n",
-    "Aqui, usamos o req.text e depois 'html.parser' que funciona como indicador do tipo do primeiro argumento (html, no caso)\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 7,
-   "metadata": {},
-   "outputs": [
-    {
-     "ename": "NameError",
-     "evalue": "name 'bs' is not defined",
-     "output_type": "error",
-     "traceback": [
-      "\u001b[1;31m---------------------------------------------------------------------------\u001b[0m",
-      "\u001b[1;31mNameError\u001b[0m                                 Traceback (most recent call last)",
-      "Cell \u001b[1;32mIn[7], line 1\u001b[0m\n\u001b[1;32m----> 1\u001b[0m \u001b[43mbs\u001b[49m(req, \u001b[38;5;124m'\u001b[39m\u001b[38;5;124mhtml.parser\u001b[39m\u001b[38;5;124m'\u001b[39m)\n",
-      "\u001b[1;31mNameError\u001b[0m: name 'bs' is not defined"
-     ]
-    }
-   ],
-   "source": [
-    "bs(req, 'html.parser')"
-   ]
-  },
-  {
-   "cell_type": "markdown",
-   "metadata": {},
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.13.0"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+from imports import *
+from main_ws import *
+
+# CONCEITOS INICIAIS
+
+# 0 - Passamos o link da página e fazemos a requisição
+link = "https://www.datascienceacademy.com.br/todoscursosdsa"
+req = requests.get(link)
+print(req.text)
+
+# 1 - site = bs(requisicao, tipo da requisição)
+#            bs(em formato de texto html, req.text, nome 'html.parser' indicando que é um arquivo html)
+site = bs(req.text, 'html.parser') 
+
+
+
+# 2 - print(site.prettify())
+#     print(site(de um jeito mais bonito e visual()))
+print(site.prettify())
+
+# 3 - site.encontre(entrada de dados)
+titulo = site.find("title")
+print(titulo)
+div = site.find("div")
+print(div)
+input = site.find("input")
+print(input)
+
+# 4 - site.encontre(todas entradas de dados na página) retorna uma lista bs4
+input_all = site.find_all("input")
+print(input_all)
+
+
+
+# DICIONÁRIO DE VARIÁVEIS
+
+cursos 
+# está no arquivo imports e significa apenas a parte do código HTML que contém 
+
+dados
+# Dados de nome do curso e preço zipados, após pegarmos do site
+
+classe_curso
+# classe específica que nos dá o código que possui o nome de cada curso
+
+classe_preco 
+# Nos dá todo o código que tem como informações os preços e suas diferentes estruturas no produto
+
+classe_promocional 
+# classe específica na parte dos preços que nos dá se a condição de curso com promoção foi atendida
+
+classe_ch
+# classe específica da carga horária dos cursos
+
+precos_div
+# É todo o código que contém os elementos dos preços, seja promocional, gratuito ou não.
+
+
+
+
+
+
+
+
