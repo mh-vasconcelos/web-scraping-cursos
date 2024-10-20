c247161fbc16736e4690b4ee58191f523ee7c6e4
mh-vasconcelos <66500702+mh-vasconcelos@users.noreply.github.com>
2024-10-19 21:05:11 -0300

    Kaggle Notebook | notebookca71515c17 | Version 2

--- main_ws.py
+++ main_ws.py
@@ -3,21 +3,21 @@
   {
    "cell_type": "code",
    "execution_count": 1,
-   "id": "2e7642fd",
+   "id": "d9dde6f9",
    "metadata": {
     "_cell_guid": "b1076dfc-b9ad-4769-8c92-a6c4dae69d19",
     "_uuid": "8f2839f25d086af736a60e9eeb907d3b93b6e0e5",
     "execution": {
-     "iopub.execute_input": "2024-10-20T00:03:48.525247Z",
-     "iopub.status.busy": "2024-10-20T00:03:48.524837Z",
-     "iopub.status.idle": "2024-10-20T00:03:49.415636Z",
-     "shell.execute_reply": "2024-10-20T00:03:49.414660Z"
+     "iopub.execute_input": "2024-10-20T00:04:58.122138Z",
+     "iopub.status.busy": "2024-10-20T00:04:58.121136Z",
+     "iopub.status.idle": "2024-10-20T00:04:59.040989Z",
+     "shell.execute_reply": "2024-10-20T00:04:59.039664Z"
     },
     "papermill": {
-     "duration": 0.89667,
-     "end_time": "2024-10-20T00:03:49.418210",
+     "duration": 0.926847,
+     "end_time": "2024-10-20T00:04:59.043926",
      "exception": false,
-     "start_time": "2024-10-20T00:03:48.521540",
+     "start_time": "2024-10-20T00:04:58.117079",
      "status": "completed"
     },
     "tags": []
@@ -72,14 +72,14 @@
   },
   "papermill": {
    "default_parameters": {},
-   "duration": 4.062461,
-   "end_time": "2024-10-20T00:03:49.839694",
+   "duration": 5.050951,
+   "end_time": "2024-10-20T00:04:59.568171",
    "environment_variables": {},
    "exception": null,
    "input_path": "__notebook__.ipynb",
    "output_path": "__notebook__.ipynb",
    "parameters": {},
-   "start_time": "2024-10-20T00:03:45.777233",
+   "start_time": "2024-10-20T00:04:54.517220",
    "version": "2.6.0"
   }
  },
